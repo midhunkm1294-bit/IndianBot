4141a8d121f50068afe3581862a5c2aa5e9376e9
Midhun K M <midhunkm1294.stark@gmail.com>
2020-05-15 17:42:14 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,11 +13,11 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
-                     f"`My peru owner`: {DEFAULTUSER}\n\n"
+    await alive.edit("**Apun Zinda He Sarr I am Alive As usual ^.^** \n`BOT Status : ` **Active - 100%**\n\n"
+                     f"`My INDIAN owner`: {DEFAULTUSER}\n\n"
                      "`Telethon version:` **6.0.9**\n`Python:` **3.7.4**\n"
-                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+                     "`Database Status:` **ALL OK NO BUG FOUND**\n\n`Always with you, my master!\n`"
+                     "**Bot Creator:** [MIDHUNKM](t.me/MARIODEVS)\n"
+                     "**Co-Owner:** [ANONYMOUS](t.me/CARDINGKING21211)\n\n"
+                       
 
